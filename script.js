13999f0e86bea0f72c34cf5c6b25202188367ba9
Julien Az <54361213+JustFS@users.noreply.github.com>
2020-04-07 18:04:56 +0200

    take off css

--- script.js
+++ script.js
@@ -1,55 +1,18 @@
-var words = document.getElementsByClassName('word');
-var wordArray = [];
-var currentWord = 0;
+document.addEventListener('mousemove', function(e) {
+  let circle = document.createElement('span');
 
-words[currentWord].style.opacity = 1;
-for (var i = 0; i < words.length; i++) {
-  splitLetters(words[i]);
-}
+  let x = e.offsetX;
+  let y = e.offsetY;
+  circle.style.left = x + "px";
+  circle.style.top = y + "px";
 
-function changeWord() {
-  var cw = wordArray[currentWord];
-  var nw = currentWord == words.length-1 ? wordArray[0] : wordArray[currentWord+1];
-  for (var i = 0; i < cw.length; i++) {
-    animateLetterOut(cw, i);
-  }
-  
-  for (var i = 0; i < nw.length; i++) {
-    nw[i].className = 'letter behind';
-    nw[0].parentElement.style.opacity = 1;
-    animateLetterIn(nw, i);
-  }
-  
-  currentWord = (currentWord == wordArray.length-1) ? 0 : currentWord+1;
-}
+  let size = Math.random() * 100;
 
-function animateLetterOut(cw, i) {
+  circle.style.width = 60 + size + "px";
+  circle.style.height = 60 + size + "px";
+
+  document.querySelector('body').appendChild(circle);
   setTimeout(function() {
-		cw[i].className = 'letter out';
-  }, i*80);
-}
-
-function animateLetterIn(nw, i) {
-  setTimeout(function() {
-		nw[i].className = 'letter in';
-  }, 340+(i*80));
-}
-
-function splitLetters(word) {
-  var content = word.innerHTML;
-  word.innerHTML = '';
-  var letters = [];
-  for (var i = 0; i < content.length; i++) {
-    var letter = document.createElement('span');
-    letter.className = 'letter';
-    letter.innerHTML = content.charAt(i);
-    word.appendChild(letter);
-    letters.push(letter);
-  }
-  
-  wordArray.push(letters);
-}
-
-changeWord();
-setInterval(changeWord, 4000);
-
+    circle.remove();
+  }, 4000);
+});
