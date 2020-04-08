7a9264e01fcb6af5a2f7b682d68d7ccd1bcdcd71
Julien Az <54361213+JustFS@users.noreply.github.com>
2020-04-08 14:36:58 +0200

    added image

--- script.js
+++ script.js
@@ -1,18 +1,29 @@
-document.addEventListener('mousemove', function(e) {
-  let circle = document.createElement('span');
+window.onload = function() {
 
-  let x = e.offsetX;
-  let y = e.offsetY;
-  circle.style.left = x + "px";
-  circle.style.top = y + "px";
+  var fileInput = document.getElementById('fileInput');
+  var fileDisplayArea = document.getElementById('fileDisplayArea');
 
-  let size = Math.random() * 100;
 
-  circle.style.width = 60 + size + "px";
-  circle.style.height = 60 + size + "px";
+  fileInput.addEventListener('change', function(e) {
+    var file = fileInput.files[0];
+    var imageType = /image.*/;
 
-  document.querySelector('body').appendChild(circle);
-  setTimeout(function() {
-    circle.remove();
-  }, 4000);
-});
+    if (file.type.match(imageType)) {
+      var reader = new FileReader();
+
+      reader.onload = function(e) {
+        fileDisplayArea.innerHTML = "";
+
+        var img = new Image();
+        img.src = reader.result;
+
+        fileDisplayArea.appendChild(img);
+      }
+
+      reader.readAsDataURL(file);	
+    } else {
+      fileDisplayArea.innerHTML = "File not supported!"
+    }
+  });
+
+}
